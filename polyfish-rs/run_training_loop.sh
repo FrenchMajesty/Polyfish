5bdb711793a90e15bbda7669dffe28ebf799408a
HenBOMB <alan.io.nightcode@gmail.com>
2026-02-17 16:05:56 -0300

    commit

--- polyfish-rs/run_training_loop.sh
+++ polyfish-rs/run_training_loop.sh
@@ -5,8 +5,8 @@ set -e
 ITERATIONS=100
 GAMES_PER_ITER=20
 export MCTS_ITERS=200
-export RAYON_NUM_THREADS=20
-export OMP_NUM_THREADS=20 # RunPod RTX 4080 16gb VRAM 62 GB RAM
+export RAYON_NUM_THREADS=24
+export OMP_NUM_THREADS=24 # RunPod RTX 4080 16gb VRAM 62 GB RAM
 export RUST_BACKTRACE=1
 
 # Log all output to session.log while still showing on console
@@ -37,8 +37,8 @@ start_system_monitor() {
 start_system_monitor
 
 echo "Building binaries..."
-# cargo build --bin polyfish --bin self_play --release --features cuda
-cargo build --bin polyfish --bin self_play --release
+cargo build --bin polyfish --bin self_play --release --features cuda
+# cargo build --bin polyfish --bin self_play --release
 
 # Parse arguments
 FORCE_TRAIN=false
